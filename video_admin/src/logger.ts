fc8e42dc725264ec0c23ab26d89b5ffc25a96b0b
J-keven <jhonasnunes425@gmail.com>
2022-12-07 10:42:34 -0300

    add loki configs

--- video_admin/src/logger.ts
+++ video_admin/src/logger.ts
@@ -23,8 +23,8 @@ class Logger {
         defaultMeta: { service: 'video_admin' },
         transports: [
           new winston.transports.Console(),
-          new winston.transports.File({ filename: 'error.log', level: 'error' }),
-          new winston.transports.File({ filename: 'log.log', level: 'info' }),
+          new winston.transports.File({ filename: './logs/error.log', level: 'error' }),
+          new winston.transports.File({ filename: './logs/log.log', level: 'info' }),
         ],
       });
     }
